5482375c346f397c2444fd82a145d9fff8cf638e
Ramom <ra-mom2000@hotmail.com>
2025-02-13 22:02:05 -0300

    input and output buttons

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,12 @@
 import * as Dialog from '@radix-ui/react-dialog'
-import { CloseButton, Content, Overlay } from './style'
-import { X } from 'phosphor-react'
+import {
+  CloseButton,
+  Content,
+  Overlay,
+  TransactionType,
+  TransactionTypeButton,
+} from './style'
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
 
 export default function NewTransactionModal() {
   return (
@@ -19,6 +25,16 @@ export default function NewTransactionModal() {
           <input type="number" placeholder="Preço" required />
           <input type="text" placeholder="Categoria" required />
 
+          <TransactionType>
+            <TransactionTypeButton variant="income">
+              <ArrowCircleUp size={24} />
+              Entrada
+            </TransactionTypeButton>
+            <TransactionTypeButton variant="outcome">
+              <ArrowCircleDown size={24} />
+              Saída
+            </TransactionTypeButton>
+          </TransactionType>
           <button type="submit">Cadastrar</button>
         </form>
       </Content>
