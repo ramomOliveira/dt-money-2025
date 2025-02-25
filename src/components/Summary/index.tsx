65a19e11428c86e09ebf1d2d8b07cae3dd4c320c
Ramom <ra-mom2000@hotmail.com>
2025-02-24 23:26:19 -0300

    transaction summary calculation

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,7 +1,30 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
 import { SummaryCard, SummaryContainer } from './style'
+import { useContext } from 'react'
+import { TransactionsContext } from '../../contexts/TransactonsContext.'
 
 export function Summary() {
+  const { transactions } = useContext(TransactionsContext)
+
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === 'income') {
+        acc.income += transaction.price
+        acc.total += transaction.price
+      } else {
+        acc.outcome += transaction.price
+        acc.total -= transaction.price
+      }
+
+      return acc
+    },
+    {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    }
+  )
+
   return (
     <SummaryContainer>
       <SummaryCard>
@@ -10,7 +33,12 @@ export function Summary() {
           <ArrowCircleUp size={32} color="#00b37e" />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>
+          {summary.income.toLocaleString('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+          })}
+        </strong>
       </SummaryCard>
 
       <SummaryCard>
@@ -19,7 +47,12 @@ export function Summary() {
           <ArrowCircleDown size={32} color="#f75a68" />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>
+          {summary.outcome.toLocaleString('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+          })}
+        </strong>
       </SummaryCard>
 
       <SummaryCard variant="green">
@@ -28,7 +61,12 @@ export function Summary() {
           <CurrencyDollar size={32} color="#fff" />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>
+          {summary.total.toLocaleString('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+          })}
+        </strong>
       </SummaryCard>
     </SummaryContainer>
   )
